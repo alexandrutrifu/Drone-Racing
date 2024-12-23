10dfbd3144283ddefa3f2aed8a0efa83b676048f
Alexandru Trifu-Urzica <alex@192.168.1.4>
2024-12-23 20:00:12 +0200

    drone build done

--- src/lab_m1/tema2/drone/drone.cpp
+++ src/lab_m1/tema2/drone/drone.cpp
@@ -11,11 +11,27 @@ void drones::Drone::CreateDrone(const char *name, const std::vector<VertexFormat
     objects::Cube *limb1 = new objects::Cube();
     objects::Cube *limb2 = new objects::Cube();
 
-    limb1->CreateCube("limb1", glm::vec3(0, 0, 0), 2, objects::droneBaseColor);
-    limb2->CreateCube("limb2", glm::vec3(0, 0, 0), 2, objects::droneBaseColor);
+    limb1->CreateCube("limb1", glm::vec3(0, 0, 0), LIMB_LENGTH, objects::droneBaseColor);
+    limb2->CreateCube("limb2", glm::vec3(0, 0, 0), LIMB_LENGTH, objects::droneBaseColor);
 
     this->limbs.push_back(limb1);
     this->limbs.push_back(limb2);
+
+    // Create end cubes
+    objects::Cube *endCube1 = new objects::Cube();
+    objects::Cube *endCube2 = new objects::Cube();
+    objects::Cube *endCube3 = new objects::Cube();
+    objects::Cube *endCube4 = new objects::Cube();
+
+    endCube1->CreateCube("endCube1", glm::vec3(0, 0, 0), END_CUBE_LENGTH, objects::droneBaseColor);
+    endCube2->CreateCube("endCube2", glm::vec3(0, 0, 0), END_CUBE_LENGTH, objects::droneBaseColor);
+    endCube3->CreateCube("endCube3", glm::vec3(0, 0, 0), END_CUBE_LENGTH, objects::droneBaseColor);
+    endCube4->CreateCube("endCube4", glm::vec3(0, 0, 0), END_CUBE_LENGTH, objects::droneBaseColor);
+
+    this->endCubes.push_back(endCube1);
+    this->endCubes.push_back(endCube2);
+    this->endCubes.push_back(endCube3);
+    this->endCubes.push_back(endCube4);
 
     // Create the four propellers
     objects::Cube *propeller1 = new objects::Cube();
@@ -23,12 +39,10 @@ void drones::Drone::CreateDrone(const char *name, const std::vector<VertexFormat
     objects::Cube *propeller3 = new objects::Cube();
     objects::Cube *propeller4 = new objects::Cube();
 
-    propeller1->CreateCube("propeller1", glm::vec3(0, 0, 0), 1, objects::dronePropellerColor);
-    propeller2->CreateCube("propeller2", glm::vec3(0, 0, 0), 1, objects::dronePropellerColor);
-    propeller3->CreateCube("propeller3", glm::vec3(0, 0, 0), 1, objects::dronePropellerColor);
-    propeller4->CreateCube("propeller4", glm::vec3(0, 0, 0), 1, objects::dronePropellerColor);
-
-    // TODO: set initial positions
+    propeller1->CreateCube("propeller1", glm::vec3(0, 0, 0), LIMB_LENGTH / 3, objects::dronePropellerColor);
+    propeller2->CreateCube("propeller2", glm::vec3(0, 0, 0), LIMB_LENGTH / 3, objects::dronePropellerColor);
+    propeller3->CreateCube("propeller3", glm::vec3(0, 0, 0), LIMB_LENGTH / 3, objects::dronePropellerColor);
+    propeller4->CreateCube("propeller4", glm::vec3(0, 0, 0), LIMB_LENGTH / 3, objects::dronePropellerColor);
 
     this->propellers.push_back(propeller1);
     this->propellers.push_back(propeller2);
@@ -41,7 +55,7 @@ void drones::Drone::RenderDrone(Shader *shader, camera::Camera *camera, const gl
     // Render drone limbs
     {
         glm::mat4 modelMatrix = glm::mat4(1);
-        modelMatrix = glm::translate(modelMatrix, glm::vec3(0, 1, 0));
+        modelMatrix = glm::translate(modelMatrix, this->position);
         modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f));
         modelMatrix = glm::scale(modelMatrix, glm::vec3(2, 0.2f, 0.2f));
         
@@ -50,13 +64,82 @@ void drones::Drone::RenderDrone(Shader *shader, camera::Camera *camera, const gl
 
     {
         glm::mat4 modelMatrix = glm::mat4(1);
-        modelMatrix = glm::translate(modelMatrix, glm::vec3(0, 1, 0));
+        modelMatrix = glm::translate(modelMatrix, this->position);
         modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f));
         modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f, 0.2f, 2));
         
         this->limbs[1]->RenderObject(shader, modelMatrix, camera, projectionMatrix);
     }
 
-    // Render drone propellers
+    // Render drone ends
+    {
+        glm::mat4 modelMatrix = glm::mat4(1);
+        modelMatrix = glm::translate(modelMatrix, this->position + glm::vec3(0, END_CUBE_LENGTH, -PROPELLER_OFFSET));
+        
+        this->endCubes[0]->RenderObject(shader, modelMatrix, camera, projectionMatrix);
+    }
+
+    {
+        glm::mat4 modelMatrix = glm::mat4(1);
+        modelMatrix = glm::translate(modelMatrix, this->position + glm::vec3(0, END_CUBE_LENGTH, PROPELLER_OFFSET));
+        
+        this->endCubes[1]->RenderObject(shader, modelMatrix, camera, projectionMatrix);
+    }
+
+    {
+        glm::mat4 modelMatrix = glm::mat4(1);
+        modelMatrix = glm::translate(modelMatrix, this->position + glm::vec3(PROPELLER_OFFSET, END_CUBE_LENGTH, 0));
+        
+        this->endCubes[2]->RenderObject(shader, modelMatrix, camera, projectionMatrix);
+    }
+
+    {
+        glm::mat4 modelMatrix = glm::mat4(1);
+        modelMatrix = glm::translate(modelMatrix, this->position + glm::vec3(-PROPELLER_OFFSET, END_CUBE_LENGTH, 0));
+        
+        this->endCubes[3]->RenderObject(shader, modelMatrix, camera, projectionMatrix);
+    }
+
+    // Render drone propellers (north, south, east, west)
+    {
+        glm::mat4 modelMatrix = glm::mat4(1);
+        modelMatrix = glm::translate(modelMatrix, this->position + glm::vec3(0, PROPELLER_HEIGHT, -PROPELLER_OFFSET));
+        modelMatrix = glm::rotate(modelMatrix, this->propellerAngle, glm::vec3(0, 1, 0));
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f));
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f, 0.2f, 2));
+        
+        this->propellers[0]->RenderObject(shader, modelMatrix, camera, projectionMatrix);
+    }
+
+    {
+        glm::mat4 modelMatrix = glm::mat4(1);
+        modelMatrix = glm::translate(modelMatrix, this->position + glm::vec3(0, PROPELLER_HEIGHT, PROPELLER_OFFSET));
+        modelMatrix = glm::rotate(modelMatrix, this->propellerAngle, glm::vec3(0, 1, 0));
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f));
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f, 0.2f, 2));
+        
+        
+        this->propellers[1]->RenderObject(shader, modelMatrix, camera, projectionMatrix);
+    }
+    
+    {
+        glm::mat4 modelMatrix = glm::mat4(1);
+        modelMatrix = glm::translate(modelMatrix, this->position + glm::vec3(PROPELLER_OFFSET, PROPELLER_HEIGHT, 0));
+        modelMatrix = glm::rotate(modelMatrix, this->propellerAngle, glm::vec3(0, 1, 0));
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f));
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f, 0.2f, 2));
+        
+        this->propellers[2]->RenderObject(shader, modelMatrix, camera, projectionMatrix);
+    }
+
+    {
+        glm::mat4 modelMatrix = glm::mat4(1);
+        modelMatrix = glm::translate(modelMatrix, this->position + glm::vec3(-PROPELLER_OFFSET, PROPELLER_HEIGHT, 0));
+        modelMatrix = glm::rotate(modelMatrix, this->propellerAngle, glm::vec3(0, 1, 0));
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f));
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f, 0.2f, 2));
+        
+        this->propellers[3]->RenderObject(shader, modelMatrix, camera, projectionMatrix);
+    }
 
 }
--- src/lab_m1/tema2/drone/drone.h
+++ src/lab_m1/tema2/drone/drone.h
@@ -12,6 +12,12 @@
 
 using namespace std;
 
+#define PROPELLER_HEIGHT 0.19f
+#define PROPELLER_OFFSET 0.72f
+#define PROPELLER_SPEED 10.0f
+#define LIMB_LENGTH 2.0f
+#define END_CUBE_LENGTH 0.08f
+
 namespace drones
 {
     class Drone : public gfxc::SimpleScene
@@ -23,9 +29,12 @@ namespace drones
             void CreateDrone(const char *name, const std::vector<VertexFormat> &vertices, const std::vector<unsigned int> &indices);
             void RenderDrone(Shader *shader, camera::Camera *camera, const glm::mat4 &projectionMatrix);
 
-            vector<Mesh *> meshes{};
             vector<objects::Cube *> limbs{};
+            vector<objects::Cube *> endCubes{};
             vector<objects::Cube *> propellers{};
+
+            glm::vec3 position = glm::vec3(0, 1, 0);
+            float propellerAngle = 0;
     };
 }
 
